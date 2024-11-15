845372ae4d677fef87564a36958e08f9263124f9
juhongahn <juhong.ahn.dev@gmail.com>
2024-11-16 06:26:23 +0900

    refactor: remove redudent

--- src/pages/DashBoard.tsx
+++ src/pages/DashBoard.tsx
@@ -1,11 +1,6 @@
 import CustomCalendar from '@/components/CustomCalendar'
-import { useEffect } from "react"
-import { handleAllowNotification } from "../lib/notificationPermission"
 
 export default function DashBoard() {
-  useEffect(() => {
-    handleAllowNotification()
-  }, [])
   return (
     <main>
       <header>header</header>
