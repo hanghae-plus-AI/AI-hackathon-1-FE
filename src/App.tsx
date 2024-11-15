1ef0efdf80b29c0377923f8bf644a32371d8e562
juhongahn <juhong.ahn.dev@gmail.com>
2024-11-16 06:42:57 +0900

    feat: add request permission

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,36 @@ import Root from "./pages/Root"
 import DashBoard from "./pages/DashBoard"
 import Signin from "./pages/Signin"
 import Signup from "./pages/Signup"
+import { getToken, onMessage } from "firebase/messaging"
+import { messaging } from "./lib/firebase"
+import { useEffect } from "react"
 
 function App() {
+  async function requestPermission() {
+    //requesting permission using Notification API
+    const permission = await Notification.requestPermission();
+
+    if (permission === "granted") {
+      const token = await getToken(messaging, {
+        vapidKey: import.meta.env.VITE_FCM_VAPID_KEY,
+      });
+
+      //We can send token to server
+      console.log("Token generated : ", token);
+    } else if (permission === "denied") {
+      //notifications are blocked
+      alert("You denied for the notification");
+    }
+  }
+
+  onMessage(messaging, (payload) => {
+    console.log(payload)
+  });
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Root />} />
