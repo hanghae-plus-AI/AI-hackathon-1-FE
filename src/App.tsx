fa6c53eaaf68d41094e31700ad1fefc7221139a6
juhongahn <juhong.ahn.dev@gmail.com>
2024-11-16 09:52:11 +0900

    feat: add push noti

--- src/App.tsx
+++ src/App.tsx
@@ -4,35 +4,13 @@ import './App.css'
 import DashBoard from './pages/DashBoard'
 import Signin from './pages/Signin'
 import Signup from './pages/Signup'
-import { getToken, onMessage } from 'firebase/messaging'
-import { messaging } from './lib/firebase'
+import { setupNotifications } from './lib/firebase'
 import { useEffect } from 'react'
 
 function App() {
-  async function requestPermission() {
-    //requesting permission using Notification API
-    const permission = await Notification.requestPermission()
-
-    if (permission === 'granted') {
-      const token = await getToken(messaging, {
-        vapidKey: import.meta.env.VITE_FCM_VAPID_KEY
-      })
-
-      //We can send token to server
-      console.log('Token generated : ', token)
-    } else if (permission === 'denied') {
-      //notifications are blocked
-      alert('You denied for the notification')
-    }
-  }
-
-  onMessage(messaging, (payload) => {
-    console.log(payload)
-  })
-
   useEffect(() => {
-    requestPermission()
-  }, [])
+    setupNotifications();
+  }, []);
 
   return (
     <Routes>
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app"
-import { getMessaging } from "firebase/messaging"
+import { getMessaging, getToken, onMessage } from "firebase/messaging"
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FCM_API_KEY,
@@ -11,4 +11,28 @@ const firebaseConfig = {
 }
 
 const app = initializeApp(firebaseConfig)
-export const messaging = getMessaging(app)
+const messaging = getMessaging(app)
+
+const setupNotifications = async () => {
+  try {
+    // Request permission for notifications
+    const permission = await Notification.requestPermission();
+    
+    if (permission === 'granted') {
+      console.log('Notification permission granted.');
+      // Get the FCM token
+      const token = await getToken(messaging);
+      console.log('FCM Token:', token);
+    } else {
+      console.log('Notification permission denied.');
+    }
+    // Handle foreground notifications
+    onMessage(messaging, (payload) => {
+      console.log('Foreground Message:', payload);
+      // Handle the notification or update your UI
+    });
+  } catch (error) {
+    console.error('Error setting up notifications:', error);
+  }
+};
+export { messaging, setupNotifications };
