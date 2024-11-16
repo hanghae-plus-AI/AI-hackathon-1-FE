47c2a2e739ecd1f46c244e3619339c6d7d274ed5
uujeen <rkddmlwls2@gmail.com>
2024-11-16 12:15:11 +0900

    Fix: report 수정

---
+++ src/const/task.ts
@@ -0,0 +1,130 @@
+export const tasks = [
+  {
+    id: 1,
+    title: 'AI 강의 수강',
+    type: 'work',
+    start: 1731726165000,
+    end: 1731729765000,
+    category: 'time'
+  },
+  {
+    id: 2,
+    title: '프로젝트 중간발표 준비',
+    type: 'work',
+    start: 1731733365000,
+    end: 1731740565000,
+    category: 'milestone'
+  },
+  {
+    id: 3,
+    title: '운동',
+    type: 'life',
+    start: 1731744165000,
+    end: 1731747765000,
+    category: 'time'
+  },
+  {
+    id: 4,
+    title: '알고리즘 스터디',
+    type: 'work',
+    start: 1731751365000,
+    end: 1731758565000,
+    category: 'time'
+  },
+  {
+    id: 5,
+    title: '개인 공부 시간',
+    type: 'work',
+    start: 1731762165000,
+    end: 1731769365000,
+    category: 'time'
+  },
+  {
+    id: 6,
+    title: '취미 활동',
+    type: 'life',
+    start: 1731772965000,
+    end: 1731776565000,
+    category: 'allday'
+  },
+  {
+    id: 7,
+    title: '주간 학습 목표 달성',
+    type: 'work',
+    start: 1731780165000,
+    end: 1731780165000,
+    category: 'milestone'
+  },
+  {
+    id: 11,
+    title: '11월 학습 계획 수립',
+    type: 'work',
+    start: 1729872000000, // 2024-11-01 00:00:00
+    end: 1729872000000,
+    task: 'milestone'
+  },
+  {
+    id: 12,
+    title: 'AI 프로젝트 기획',
+    type: 'work',
+    start: 1729958400000, // 2024-11-02 00:00:00
+    end: 1730044800000, // 2024-11-03 00:00:00
+    task: 'allday'
+  },
+  {
+    id: 13,
+    title: '주간 운동',
+    type: 'life',
+    start: 1730131200000, // 2024-11-04 00:00:00
+    end: 1730217600000, // 2024-11-05 00:00:00
+    task: 'time'
+  },
+  {
+    id: 14,
+    title: '중간 프로젝트 발표',
+    type: 'work',
+    start: 1730304000000, // 2024-11-06 00:00:00
+    end: 1730304000000,
+    task: 'milestone'
+  },
+  {
+    id: 15,
+    title: '알고리즘 스터디',
+    type: 'work',
+    start: 1730390400000, // 2024-11-07 00:00:00
+    end: 1730476800000, // 2024-11-08 00:00:00
+    task: 'time'
+  },
+  {
+    id: 16,
+    title: '주말 여가 활동',
+    type: 'life',
+    start: 1730563200000, // 2024-11-09 00:00:00
+    end: 1730649600000, // 2024-11-10 00:00:00
+    task: 'allday'
+  },
+  {
+    id: 17,
+    title: '딥러닝 심화 학습',
+    type: 'work',
+    start: 1730736000000, // 2024-11-11 00:00:00
+    end: 1730822400000, // 2024-11-12 00:00:00
+    task: 'time'
+  },
+  {
+    id: 18,
+    title: '프로젝트 중간 점검',
+    type: 'work',
+    start: 1730908800000, // 2024-11-13 00:00:00
+    end: 1730908800000,
+    task: 'milestone'
+  },
+  {
+    id: 19,
+    title: '개인 독서 시간',
+    type: 'life',
+    start: 1730995200000, // 2024-11-14 00:00:00
+    end: 1731081600000, // 2024-11-15 00:00:00
+    task: 'time'
+  }
+]
